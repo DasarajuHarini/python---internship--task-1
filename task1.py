6dd5b2967d7fb6495beab0c7afdb59c09162eb92
DasarajuHarini <158148525+DasarajuHarini@users.noreply.github.com>
2026-01-20 16:30:02 +0530

    Intitial commit

---
+++ task1.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+Name = input("Enter your name: ")
+Role = input("Enter your internship role: ")
+
+Today_date = date.today()
+
+print("\n--- Internship Details ---")
+print("Name:", Name)
+print("Role:", Role)
+print("Date:", Today_date)
